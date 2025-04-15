e7328f18a190f34d55410f032387823218acddda
Sergiootl <sergioterrazaslobato@gmail.com>
2025-04-15 12:21:07 +0200

    Datos Sintéticos

--- Recomendador.py
+++
@@ -1,9 +0,0 @@
-class Recomendador:
-    def __init__(self):
-        pass
-
-    def recomendar(self, ciudad, plato):
-        restaurantes = ciudad.obtener_restaurantes_por_plato(plato)
-        # Ordenar por la valoración
-        restaurantes_ordenados = sorted(restaurantes, key=lambda x: x.valoracion, reverse=True)
-        return restaurantes_ordenados[:3]  # Devolver los tres mejores
